67f500efbbc4ebd1b590f588442660a00d5dd3c1
krashi2016 <krashi2016@gmail.com>
2020-02-01 06:12:36 -0500

    commit

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
-app.get('/Mars', function (req, res) {
+oc -app.get('/Mars', function (req, res) {
   res.send('Hello Mars!!!!\n');
 });
 
